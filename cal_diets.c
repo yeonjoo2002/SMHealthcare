532b35aca0d5e5ac0330f58b89cd9b5d87782cf9
yeonjoo2002 <yeonj000616@sookmyung.ac.kr>
2024-11-26 02:59:52 +0900

    11.26 cal_diets 수정

--- cal_diets.c
+++ cal_diets.c
@@ -22,26 +22,90 @@ static Diet diet_list[MAX_DIETS];
 static int diet_list_size = 0;
 
 
+void loadDiets(const char* DIETFILEPATH) {
+    FILE *file = fopen(DIETFILEPATH, "r");   // open diets.txt
+    if (file == NULL) 
+	{
+        printf("There is no file for diets!\n");
+        return;
+    }
+
+    // read data from diets.txt and save at diet_list
+    while (fscanf(file, "%49s %d", 
+                  diet_list[diet_list_size].food_name, 
+                  &diet_list[diet_list_size].calories_intake) == 2) 
+	{
+        diet_list_size++;
+
+        if (diet_list_size >= MAX_DIETS) 
+		{
+            printf("Maximum number of diets reached.\n");
+            break; 
+        }
+    }
+    fclose(file); // close diets.txt
+}
+
+void inputDiet(HealthData* health_data) {
+    if (diet_list_size == 0) {
+        printf("No diets available. Please load diets first.\n");
+        return;
+    }
+
+    int choice;
+
+    // print diet_list 
+    printf("The list of diets:\n");
+    for (int i = 0; i < diet_list_size; i++) {
+        printf("%d. %s (%d kcal)\n", i + 1, diet_list[i].food_name, diet_list[i].calories_intake);
+    }
+    printf("%d. Exit\n", diet_list_size + 1);
+
+    // input number from user
+    printf("Choose (1-%d): ", diet_list_size + 1);
+    scanf("%d", &choice);
+
+    if (choice >= 1 && choice <= diet_list_size) 
+	{
+        // save selected diet at health_data
+        Diet selected_diet = diet_list[choice - 1];
+        if (health_data->diet_count < MAX_DIETS) 
+		{
+            health_data->diet[health_data->diet_count] = selected_diet; // save diets
+            health_data->diet_count++;
+            health_data->total_calories_intake += selected_diet.calories_intake; // update total calories
+        } 
+        
+    } 
+}
+
 /*
     description : read the information in "diets.txt"
 */
 
-void loadDiets(const char* DIETFILEPATH) {
-    FILE *file = fopen(DIETFILEPATH, "r");
+//loadDiets("C:\\Users\\a\\Desktop\\SMHealthcare\\Project\\diets.txt");
+
+/*void loadDiets(const char* DIETFILEPATH) {
+    
+	
+	FILE *file = fopen(DIETFILEPATH, "r");	
     if (file == NULL) {
         printf("There is no file for diets! \n");
         return;
     }
-
-     // ToCode: to read a list of the diets from the given file
-    while () {
-    	
+    
+	// ToCode: to read a list of the diets from the given file
+    while ()	
+	
         if (diet_list_size >= MAX_DIETS){
-        	break;
+			break;
 		}
     }
     fclose(file);
+    
+  
 }
+
 
 /*
     description : to enter the selected diet and the total calories intake in the health data
@@ -52,7 +116,7 @@ void loadDiets(const char* DIETFILEPATH) {
     			2. enter the selected diet and the total calories intake in the health data
 */
 
-void inputDiet(HealthData* health_data) {
+/*void inputDiet(HealthData* health_data) {
     int choice, i;
     
     // ToCode: to provide the options for the diets to be selected
@@ -69,4 +133,4 @@ void inputDiet(HealthData* health_data) {
 
 
 }
-
+*/
