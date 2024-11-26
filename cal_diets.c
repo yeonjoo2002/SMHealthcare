139ea4899e64b917a0fb98751059047077f8d219
yeonjoo2002 <yeonj000616@sookmyung.ac.kr>
2024-11-26 14:52:53 +0900

    2024.11.26_2

--- cal_diets.c
+++ cal_diets.c
@@ -17,7 +17,13 @@
 #define MAX_FOOD_NAME_LEN 50		// Maximum length of the name of food
 
 
-// list of diets 
+/*
+	typedef struct 
+	{	char food_name[MAX_FOOD_NAME_LEN]; 	// the name of food
+    	int calories_intake;   				// calorie intake
+	} 	Diet;
+*/
+
 static Diet diet_list[MAX_DIETS];
 static int diet_list_size = 0;
 
@@ -29,17 +35,17 @@ void loadDiets(const char* DIETFILEPATH) {
         printf("There is no file for diets!\n");
         return;
     }
-
+    
+    // ToCode: to read a list of the diets from the given file
     // read data from diets.txt and save at diet_list
     while (fscanf(file, "%49s %d", 
                   diet_list[diet_list_size].food_name, 
                   &diet_list[diet_list_size].calories_intake) == 2) 
 	{
-        diet_list_size++;
+        diet_list_size++;	// increament diet_list_size when the loop begin
 
-        if (diet_list_size >= MAX_DIETS) 
+        if (diet_list_size >= MAX_DIETS)	// break the loop when the diet_list_size >= MAX_DIETS
 		{
-            printf("Maximum number of diets reached.\n");
             break; 
         }
     }
@@ -47,65 +53,97 @@ void loadDiets(const char* DIETFILEPATH) {
 }
 
 void inputDiet(HealthData* health_data) {
-    if (diet_list_size == 0) {
-        printf("No diets available. Please load diets first.\n");
-        return;
-    }
+    int choice,i;
 
-    int choice;
-
-    // print diet_list 
+    // ToCode: to provide the options for the diets to be selected
     printf("The list of diets:\n");
-    for (int i = 0; i < diet_list_size; i++) {
+    
+    // display diets.txt using diet_list
+    for (i = 0; i < diet_list_size; i++) {
         printf("%d. %s (%d kcal)\n", i + 1, diet_list[i].food_name, diet_list[i].calories_intake);
     }
+    
+    // ToCode: to enter the diet to be chosen with exit option
     printf("%d. Exit\n", diet_list_size + 1);
 
-    // input number from user
-    printf("Choose (1-%d): ", diet_list_size + 1);
-    scanf("%d", &choice);
+    printf("Choose (1-%d): ", diet_list_size + 1); 	
+    scanf("%d", &choice);	// choose diet what the user wants
 
-    if (choice >= 1 && choice <= diet_list_size) 
+	// ToCode: to enter the selected diet in the health data
+    if (choice >= 1 && choice <= diet_list_size)	// the number of provided diets is diet_list_size
 	{
-        // save selected diet at health_data
-        Diet selected_diet = diet_list[choice - 1];
         if (health_data->diet_count < MAX_DIETS) 
 		{
-            health_data->diet[health_data->diet_count] = selected_diet; // save diets
-            health_data->diet_count++;
-            health_data->total_calories_intake += selected_diet.calories_intake; // update total calories
+            health_data->diet[health_data->diet_count] = diet_list[choice-1]; // save the number of diets (that user enters) at health_data
+            health_data->diet_count++;	// sice user enters diets, increment diet_count and save at health_data
+            
+			// ToCode: to enter the total calories intake in the health data
+            health_data->total_calories_intake += diet_list[choice-1].calories_intake;	// update total intaken calories 
         } 
         
     } 
 }
 
+
+/*
+void test_HealthData(const HealthData* health_data) {
+    printf("\n---- Health Data ----\n");
+    printf("Total Calories Intake: %d kcal\n", health_data->total_calories_intake);
+
+    if (health_data->diet_count > 0) {
+        printf("\nDiet History (%d items):\n", health_data->diet_count);
+        for (int i = 0; i < health_data->diet_count; i++) {
+            printf("%d. Food: %s, Calories: %d kcal\n",
+                   i + 1,
+                   health_data->diet[i].food_name,
+                   health_data->diet[i].calories_intake);
+        }
+    } else {
+        printf("\nDiet History: No diets recorded yet.\n");
+    }
+
+    printf("----------------------\n");
+}
+
+int main() {
+    HealthData health_data = {0};
+
+    loadDiets("C:\\Users\\a\\Desktop\\Final Project\\diets.txt");
+
+    while (1) {
+        printf("\n---- Diet Input ----\n");
+        inputDiet(&health_data);
+        test_HealthData(&health_data);
+    }
+
+    return 0;
+}
+*/
+
+
+
+
+
 /*
     description : read the information in "diets.txt"
 */
 
-//loadDiets("C:\\Users\\a\\Desktop\\SMHealthcare\\Project\\diets.txt");
-
 /*void loadDiets(const char* DIETFILEPATH) {
-    
-	
-	FILE *file = fopen(DIETFILEPATH, "r");	
+    FILE *file = fopen(DIETFILEPATH, "r");
     if (file == NULL) {
         printf("There is no file for diets! \n");
         return;
     }
-    
-	// ToCode: to read a list of the diets from the given file
-    while ()	
-	
+
+     // ToCode: to read a list of the diets from the given file
+    while () {
+    	
         if (diet_list_size >= MAX_DIETS){
-			break;
+        	break;
 		}
     }
     fclose(file);
-    
-  
 }
-
 
 /*
     description : to enter the selected diet and the total calories intake in the health data
@@ -116,7 +154,8 @@ void inputDiet(HealthData* health_data) {
     			2. enter the selected diet and the total calories intake in the health data
 */
 
-/*void inputDiet(HealthData* health_data) {
+/*
+void inputDiet(HealthData* health_data) {
     int choice, i;
     
     // ToCode: to provide the options for the diets to be selected
--- cal_exercise.c
+++ cal_exercise.c
@@ -16,17 +16,122 @@
 #define MAX_EXERCISES 100  			// Maximum number of exercises
 #define MAX_EXERCISE_NAME_LEN 50	// Maximum length of the name of exercise
 
+/*typedef struct {
+    char exercise_name[MAX_EXERCISE_NAME_LEN];	// the name of the exercise
+    int calories_burned_per_minute;   			// calory burned per minute
+} Exercise;
+*/
 
 // To declare the structure of the exercises
 static Exercise exercise_list[MAX_EXERCISES];
 int exercise_list_size = 0;
 
+void loadExercises(const char* EXERCISEFILEPATH) {
+    FILE *file = fopen(EXERCISEFILEPATH, "r");	// open exercises.txt
+    if (file == NULL) 
+	{
+        printf("There is no file for exercises! \n");
+        return;
+    }
+
+    // ToCode: to read a list of the exercises from the given file
+   	// read data from exercises.txt and save at diet_list
+    while (fscanf(file, "%49s %d", 
+                  exercise_list[exercise_list_size].exercise_name, 
+                  &exercise_list[exercise_list_size].calories_burned_per_minute) == 2) 
+	{
+        exercise_list_size++;	// increament exercise_list_size when the loop begin
+
+        if (exercise_list_size >= MAX_EXERCISES)	// break the loop when the exercise_list_size >= MAX_EXERCISES
+		{
+        	break;
+		}
+    }
+
+    fclose(file);	// close exercises.txt
+}
+
+
+void inputExercise(HealthData* health_data) {
+    int choice, duration, i;
+    int calories_burned;	// since user enters duration, we need new variables that indicates total caloreis burned through exercise
+    
+    // ToCode: to provide the options for the exercises to be selected
+    printf("The list of exercises: \n");
+    
+    // display exercises.txt using exercise_list
+	for (i = 0; i < exercise_list_size; i++) {
+        printf("%d. %s (%d kcal burned per min.)\n", i + 1, exercise_list[i].exercise_name, exercise_list[i].calories_burned_per_minute);
+    }
+    
+    // ToCode: to enter the exercise to be chosen with exit option
+	printf("%d. Exit\n", exercise_list_size + 1);
+
+    printf("Choose (1-%d): ", exercise_list_size + 1);
+    scanf("%d", &choice); 	// choose exercise what the user wants
+    
+    // To enter the duration of the exercise
+    printf("Enter the duration of the exercise (in min.): ");
+    scanf("%d", &duration);	// choose duration what the user wants
+    
+    // ToCode: to enter the selected exercise and total calcories burned in the health data 
+    if (choice >= 1 && choice <= exercise_list_size)	// the number of provided exercises is exercise_list_size
+	{
+        calories_burned = exercise_list[choice - 1].calories_burned_per_minute * duration;	// calculate calories through exercise
+        
+        if (health_data->exercise_count < MAX_EXERCISES) 
+		{
+        health_data->exercises[health_data->exercise_count] = exercise_list[choice - 1];  // save the number of exercises (that user enters) at health_data
+        health_data->exercise_count++;	// sice user enters exercises, increment exercise_count and save at health_data
+        
+        health_data->total_calories_burned += calories_burned; 	// update total burned calories 
+    	}
+        
+    } 
+
+}
+
+
+/*void test_HealthData(const HealthData* health_data) {
+    printf("\n---- Health Data ----\n");
+    printf("Total Calories Intake: %d kcal\n", health_data->total_calories_intake);
+
+    if (health_data->exercise_count > 0) {
+        printf("\nExercise History (%d items):\n", health_data->exercise_count);
+        for (int i = 0; i < health_data->exercise_count; i++) {
+            printf("%d. exercise: %s, Calories: %d kcal\n",
+                   i + 1,
+                   health_data->exercises[i].exercise_name,
+                   health_data->exercises[i].calories_burned_per_minute);
+        }
+    } else {
+        printf("\nDiet History: No diets recorded yet.\n");
+    }
+
+    printf("----------------------\n");
+}
+
+int main() {
+    HealthData health_data = {0};
+
+    loadExercises("C:\\Users\\a\\Desktop\\Final Project\\exercises.txt");
+
+    while (1) {
+        printf("\n---- Exerciese Input ----\n");
+        inputExercise(&health_data);
+        test_HealthData(&health_data);
+    }
+
+    return 0;
+}
+
+*/
 
 /*
     description : read the information in "excercises.txt"
 */
 
-void loadExercises(const char* EXERCISEFILEPATH) {
+/*void loadExercises(const char* EXERCISEFILEPATH) {
     FILE *file = fopen(EXERCISEFILEPATH, "r");
     if (file == NULL) {
         printf("There is no file for exercises! \n");
@@ -55,7 +160,7 @@ void loadExercises(const char* EXERCISEFILEPATH) {
     			3. enter the selected exercise and the total calories burned in the health data
 */
 
-void inputExercise(HealthData* health_data) {
+/*void inputExercise(HealthData* health_data) {
     int choice, duration, i;
     
     // ToCode: to provide the options for the exercises to be selected
@@ -74,3 +179,4 @@ void inputExercise(HealthData* health_data) {
     
 
 }
+*/
